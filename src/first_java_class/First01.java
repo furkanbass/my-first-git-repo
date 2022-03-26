371a635aad9931d87e1970d662fdeee7f423b994
FURKAN <furkanbas0626@gmail.com>
2022-03-26 15:56:31 -0400

    Made some changes

--- src/first_java_class/First01.java
+++ src/first_java_class/First01.java
@@ -2,6 +2,8 @@ package first_java_class;
 
 public class First01 {
     public static void main(String[] args) {
-        System.out.println("Hello github");
+        String hello = "Hello github";
+        System.out.println(hello);
+
     }
 }
